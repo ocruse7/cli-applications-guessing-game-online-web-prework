44311693b21f48bdaa15a49dc074f501f9b63a7c
Owen Cruse <ocruse7@gmail.com>
2019-10-28 05:20:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,6 +2,7 @@ def run_guessing_game
   answer = rand(6)+1
   puts "Guess a number between 1 and 6:"
   input = gets.chomp
+  
   if input == answer.to_s
     puts "You guessed the correct number!"
   elsif input.downcase == "exit"
@@ -9,4 +10,5 @@ def run_guessing_game
   else input != answer
     puts "Sorry! The computer guessed #{answer}."
   end
+  
 end
